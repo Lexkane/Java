5ecf35065f8a044552c97cddd12c9e8cb4d19774
Lexkane <40840623+Lexkane@users.noreply.github.com>
2018-10-27 23:38:17 +0300

    Add files via upload

---
+++ JavaFileReader/JavaFileReader.java
@@ -0,0 +1,45 @@
+//Read from file in Java
+
+package MySoft;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String [] args) {
+
+        // The name of the file to open.
+        String fileName = "temp.txt";
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader =
+                    new FileReader(fileName);
+
+            // Wraping FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            fileName + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + fileName + "'");
+
+            }
+    }
+}
+
